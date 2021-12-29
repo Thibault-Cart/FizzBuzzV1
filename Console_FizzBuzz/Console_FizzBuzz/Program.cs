24a375a9b3784e05237deb39e5910c790cb68684
ThibaultCart <43812982+ThibaultCart@users.noreply.github.com>
2021-12-29 13:44:04 +0100

    d

---
+++ Console_FizzBuzz/Console_FizzBuzz/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 0; i <= 100; i++)
+            {
+                if (i%3==0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                if (i % 3 == 0&& i % 5 == 0)
+                {
+                    Console.WriteLine("FIZZ BUZZ");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
